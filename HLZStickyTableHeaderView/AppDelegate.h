ecff964b68e3413fc65a22f22765d23c9e5ee14e
Hu Lizhen <hulizhen.public@gmail.com>
2016-06-13 19:45:13 +0800

    Initial Commit

---
+++ HLZStickyTableHeaderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLZStickyTableHeaderView
+//
+//  Created by Hu Lizhen on 6/13/16.
+//  Copyright © 2016 hulizhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLZStickyTableHeaderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLZStickyTableHeaderView
+//
+//  Created by Hu Lizhen on 6/13/16.
+//  Copyright © 2016 hulizhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
